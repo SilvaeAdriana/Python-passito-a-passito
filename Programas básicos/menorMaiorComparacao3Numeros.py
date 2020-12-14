bb33c504a50178c775ce4a91780633118cb2cba9
SilvaeAdriana <75712203+SilvaeAdriana@users.noreply.github.com>
2020-12-13 22:43:45 -0300

    Create menorMaiorComparacao3Numeros.py
    Usuário digita 3 números  e o programa compara e imprime o menor e maior valores digitados.

---
+++ Programas básicos/menorMaiorComparacao3Numeros.py
@@ -0,0 +1,17 @@
+n1=int(input("Insira o primeiro número:"))
+n2=int(input("Insira o segundo número:"))
+n3=int(input("Insira o terceiro número:"))
+if n1<n2 and n1<n3:
+   menor=n1
+if n2<n1 and n2<n3:
+   menor=n2
+if n3<n1 and n3<n2:
+    menor=n3
+print("O maior valor inserido é:",menor)
+if n1>n2 and n1>n3:
+   maior=n1
+if n2>n1 and n2>n3:
+   maior=n2
+if n3>n1 and n3>n2:
+   maior=n3
+print("O menor valor inserido é:",maior)
