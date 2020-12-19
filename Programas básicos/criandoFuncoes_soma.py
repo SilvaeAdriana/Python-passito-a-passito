2ec02f8096b3791a09b91e4feb2a26320e1d3a55
SilvaeAdriana <75712203+SilvaeAdriana@users.noreply.github.com>
2020-12-19 12:03:28 -0300

    Create criandoFuncoes_soma.py
    Função soma: recebendo 2 numeros digitados pelo usuário ,retorna a soma entre eles. Duas alternativas.

---
+++ Programas básicos/criandoFuncoes_soma.py
@@ -0,0 +1,11 @@
+#alternativa 1
+def soma(a,b):
+    print(a+b)
+soma(7,2)
+
+#alternativa 2
+n1=int(input('Insira um primeiro número:'))
+n2=int(input('Insira um segundo número:'))
+def soma(n1,n2):
+    print(n1+n2)   
+soma(n1,n2)
